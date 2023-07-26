f835084756f67cc9ee1a781aee0db9a3cbdc827b
Kajal <Kajal.mittal08@gmail.com>
2023-07-26 20:26:27 +0530

    Added View

--- src/view/DeleteMenuItem.java
+++ src/view/DeleteMenuItem.java
@@ -59,7 +59,68 @@ public class DeleteMenuItem extends JFrame{
 		JLabel lblDeleteMenuItemFrom=new JLabel("Delete Item From Menu");
 		lblDeleteMenuItemFrom.setForeground(Color.BLACK);
 		lblDeleteMenuItemFrom.setFont(new Font("Calibri", Font.BOLD, 30));
+		lblDeleteMenuItemFrom.setBounds(556, 72, 318, 53);
+		contentPane.add(lblDeleteMenuItemFrom);
 		
+		JLabel lblItemPrice=new JLabel("Item Name");
+		lblItemPrice.setForeground(Color.BLACK);
+		lblItemPrice.setFont(new Font("Calibri", Font.BOLD, 20));
+		lblItemPrice.setBounds(280, 216, 103, 32);
+		contentPane.add(lblItemPrice);
+		
+		textField=new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(439, 216, 234, 40);
+		contentPane.add(textField);
+		
+		
+		lblNewLabel=new JLabel("");
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 20));
+		lblNewLabel.setBounds(585, 455, 289, 32);
+		contentPane.add(lblNewLabel);
+		
+		btnDelete=new JButton("Delete");
+		btnDelete.setBackground(Color.BLACK);
+		btnDelete.setForeground(Color.WHITE);
+		btnDelete.setFont(new Font("Calibri", Font.BOLD, 22));
+		btnDelete.setBounds(648, 360, 128, 60);
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				boolean result=obj_Menu.DeleteMenuItem(textField.getText());
+				if(result==true) {
+					lblNewLabel.setText("Item Removed Successfully");
+	
+				}
+				else {
+					lblNewLabel.setText("Deletion Unsuccessful");
+					
+				}
+			}
+		});
+		contentPane.add(btnDelete);
+		btnExit=new JButton("Back");
+		btnExit.setForeground(Color.WHITE);
+		btnExit.setBackground(Color.BLACK);
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(true);
+				dispose();
+			}
+		});
+		
+		contentPane.add(btnExit);
+		
+		btnNewButton=new JButton("Exit");
+		btnNewButton.setBackground(Color.BLACK);
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setFont(new Font("Calibri", Font.BOLD, 22));
+		btnNewButton.setBounds(1124, 564, 111, 53);
+		contentPane.add(btnNewButton);
+		
+		label=new JLabel("My Cafe");
+		label.setFont(new Font("Mathura MT Script Capitals",Font.BOLD, 20));
+		label.setBounds(10,11, 182, 32);
+		contentPane.add(label);
 	}
 
 }
